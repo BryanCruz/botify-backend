886b4cdbffd94162e766de6009f0e71816ce897f
BryanCruz <bryan.bialokur@gmail.com>
2020-05-21 16:41:25 -0300

    feat: add commands to invoke and dismiss bot

--- src/discord/client.ts
+++ src/discord/client.ts
@@ -23,11 +23,37 @@ const getCommand = (message: Discord.Message): command | null => {
   return { command: regexTest[1].toLowerCase(), params: regexTest[2].trim() };
 };
 
+const getVoiceChannel = (message: Discord.Message): Discord.VoiceChannel => {
+  const authorGuildMember = message.author.presence.member;
+  const voiceChannel = authorGuildMember.voice.channel;
+
+  return voiceChannel;
+};
+const connectToVoice = (message: Discord.Message) => {
+  const voiceChannel = getVoiceChannel(message);
+
+  voiceChannel.join();
+};
+
+const disconnectFromVoice = (message: Discord.Message) => {
+  const voiceChannel = getVoiceChannel(message);
+
+  voiceChannel.leave();
+};
+
 client.on("message", (message) => {
   const { command, params } = getCommand(message);
 
   if (!command) {
     return;
+  }
+
+  if (command === "invoke") {
+    connectToVoice(message);
+  }
+
+  if (command === "leave") {
+    disconnectFromVoice(message);
   }
 });
 
