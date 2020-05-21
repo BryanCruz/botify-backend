2caaed744df4ea6d5da9314587d62d45f00bc5bd
BryanCruz <bryan.bialokur@gmail.com>
2020-05-21 16:16:57 -0300

    feat: improve command recognition

--- src/discord/client.ts
+++ src/discord/client.ts
@@ -1,28 +1,30 @@
 import Discord from "discord.js";
 import config from "../config";
 
+type command = { command: string; params: string };
+
 const client = new Discord.Client();
 
 client.login(config.discordToken);
 
-const getCommand = (message: Discord.Message): string | null => {
+const getCommand = (message: Discord.Message): command | null => {
+  const emptyCommand: command = { command: null, params: null };
+
   if (message.author.bot || message.channel.type !== "text") {
-    return null;
+    return emptyCommand;
   }
 
-  const regexTest = message.content.match(/\$(.*)/);
+  const regexTest = message.content.match(/\$ *([a-zA-Z]*)(.*)/);
 
   if (!regexTest) {
-    return null;
+    return emptyCommand;
   }
 
-  const command = regexTest[1].trim();
-  return command;
+  return { command: regexTest[1].toLowerCase(), params: regexTest[2].trim() };
 };
 
 client.on("message", (message) => {
-  const command = getCommand(message);
-  console.log(command);
+  const { command, params } = getCommand(message);
 
   if (!command) {
     return;
