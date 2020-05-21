77f895180a28793d9a7b19cde07536a425d8f648
BryanCruz <bryan.bialokur@gmail.com>
2020-05-21 15:51:33 -0300

    feat: add command interpreter

--- src/discord/client.ts
+++ src/discord/client.ts
@@ -5,4 +5,31 @@ const client = new Discord.Client();
 
 client.login(config.discordToken);
 
+const getCommand = (message: Discord.Message): string | null => {
+  if (
+    message.author.tag === client.user.tag ||
+    message.channel.type !== "text"
+  ) {
+    return null;
+  }
+
+  const regexTest = message.content.match(/\$(.*)/);
+
+  if (!regexTest) {
+    return null;
+  }
+
+  const command = regexTest[1].trim();
+  return command;
+};
+
+client.on("message", (message) => {
+  const command = getCommand(message);
+  console.log(command);
+
+  if (!command) {
+    return;
+  }
+});
+
 export default client;
