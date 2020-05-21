0a7ac358f79291ee693aaf01ba765f96bb08c405
BryanCruz <bryan.bialokur@gmail.com>
2020-05-21 18:38:40 -0300

    chore: change alias

--- src/discord/client.ts
+++ src/discord/client.ts
@@ -85,7 +85,7 @@ client.on("message", (message) => {
     disconnectFromVoice(message);
   }
 
-  if (command === "p" || command === "play") {
+  if (command === "m" || command === "meme") {
     playSomething(message, params);
   }
 });
